0f57d24d1a62fe91252c4895c5a16ecc6bc83dad
ulyssetsd <utassidis@gmail.com>
2019-05-14 22:07:39 +0100

    Init ASP.NET Core project

---
+++ simple-newsletter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace simple_newsletter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
