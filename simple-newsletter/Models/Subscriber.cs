4d6837b1c281cb5457b58090a89ae635f6fbf002
Ulysse Tassidis <Ulysse@imec.ie>
2019-05-16 14:17:10 +0100

    Update Subscriber with some properties

---
+++ simple-newsletter/Models/Subscriber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simple_newsletter.Models
+{
+    public class Subscriber
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Reason { get; set; }
+        public string Heard { get; set; }
+    }
+}
