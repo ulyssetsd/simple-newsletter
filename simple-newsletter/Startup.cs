4129be1463df46a0e8ee6db68882e88342f5f382
ulyssetsd <utassidis@gmail.com>
2019-05-17 22:04:26 +0100

    Update startup for production

--- simple-newsletter/Startup.cs
+++ simple-newsletter/Startup.cs
@@ -28,8 +28,16 @@ namespace simple_newsletter
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddDbContext<simple_newsletterContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("simple_newsletterContext")));
+            // Use SQL Database if in Azure, otherwise, use local sqlserver
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+                services.AddDbContext<simple_newsletterContext>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+            else
+                services.AddDbContext<simple_newsletterContext>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("simple_newsletterContext")));
+
+            // Automatically perform database migration
+            services.BuildServiceProvider().GetService<simple_newsletterContext>().Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
